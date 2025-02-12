a02d1568ac48ac6bc55603d40a4c38828bc69282
Lucas pereira <lucasgunner3@gmail.com>
2025-02-12 08:53:49 -0300

    refactor: melhorando legibilidade

--- tcp-server.py
+++ tcp-server.py
@@ -4,15 +4,17 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
 port = 3535
+allowedNumberOfConnections = 5
 
 serverSocket.bind((host, port))
 
-serverSocket.listen(5)
+serverSocket.listen(allowedNumberOfConnections)
 
 while True:
     clientSocket, addr = serverSocket.accept()
     
     print("Got a connection from %s" % str(addr))
+
     clientSocket.send("Thank you for connecting")
 
     clientSocket.close()
